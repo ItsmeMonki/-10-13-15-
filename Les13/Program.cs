3ed4e24c136be30f6ea5e23cfe035d19030b3172
Vrezh <vrezhikmarkaryan@gmail.com>
2022-09-03 12:31:28 +0500

    Les13

--- Les13/Program.cs
+++ Les13/Program.cs
@@ -3,12 +3,28 @@
 //78 -> третьей цифры нет
 //32679 -> 6
 
-Console.WriteLine("Введите трехзначное число: ");
+Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
+int n = num;
+int count = 0;
 
-int n1 = num / 10;
-int n2 = num % 10;
-int n3 = num % 10;
-int result = n3 = n2 % 10;
+while (n != 0){ //поиск общего количества знаков
+    n = n/10;
+    count++;
+}
+int remDiv = 1; //вычисление, от какого числа надо искать остаток деления
+for (int i = 2; i < count; i++){
+    remDiv *= 10;
+}
+int div = 1; //переменная для деления и отбрасывания лишних цифр справа
 
-Console.WriteLine($"{result} -> Третье число");
+if (count < 3){
+    Console.WriteLine("Третьей цифры нет");
+} else {
+    while(count > 3){
+        div *= 10;
+        count--;
+    }
+    int n1 = (num % remDiv)/div;
+    Console.WriteLine(n1);
+}
